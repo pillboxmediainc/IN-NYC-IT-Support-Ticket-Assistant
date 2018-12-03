de7c15891fbfd55b4e10db232e047df40f607a71
Josh Feinstein <pillboxmediainc@gmail.com>
2018-12-02 22:22:11 -0500

    ready for first upload to chrome store

--- innycit-support.js
+++ innycit-support.js
@@ -2,41 +2,25 @@ const emailUrl = 'mailto:support@innycit.com?subject=New%20Support%20Ticket%20-%
 const tipId = document.getElementById('it-tips-id');
 const tipTitle = document.getElementById('it-tips-title');
 const tipBody = document.getElementById('it-tips-body');
+const button = document.getElementById('create-ticket')
+
 const shortcutsTable = `<div align="center"><table width="80%" align="center"><tbody><tr><td>&nbsp;</td><td><u>Mac</u></td><td><u>PC</u></td></tr><tr><td>Cut:</td><td>Cmd-X</td><td>Cntrl-X</td></tr><tr><td>Copy:</td><td>Cmd-C</td><td>Cntrl-C</td></tr><tr><td>Past:</td><td>Cmd-V</td><td>Cntrl-V</td></tr></tbody></table></div>`
 const tips = [
   { id: 01, title: "When in Doubt, Restart It", body: "It's often overlooked, but sometimes the best first course of action is to power cycle the device in question." },
   { id: 02, title: "Cut, Copy, Paste Keyboard Shortcuts:", body: `Save time by using keyboard shortcuts<br><br>${shortcutsTable}` },
-  // { id: 03, title: "title 03", body: "body 03" },
-  // { id: 04, title: "title 04", body: "body 04" },
-  // { id: 05, title: "title 05", body: "body 05" },
-  // { id: 06, title: "title 06", body: "body 06" },
-  // { id: 07, title: "title 07", body: "body 07" },
-  // { id: 08, title: "title 08", body: "body 08" },
-  // { id: 09, title: "title 09", body: "body 09" },
-  // { id: 10, title: "title 10", body: "body 10" },
-  // { id: 11, title: "title 11", body: "body 11" },
-  // { id: 12, title: "title 12", body: "body 12" },
-  // { id: 13, title: "title 13", body: "body 13" },
-  // { id: 14, title: "title 14", body: "body 14" },
-  // { id: 15, title: "title 15", body: "body 15" },
-  // { id: 16, title: "title 16", body: "body 16" },
-  // { id: 17, title: "title 17", body: "body 17" },
-  // { id: 18, title: "title 18", body: "body 18" },
-  // { id: 19, title: "title 19", body: "body 19" },
-  // { id: 20, title: "title 20", body: "body 20" },
-  // { id: 21, title: "title 21", body: "body 21" },
-  // { id: 22, title: "title 22", body: "body 22" },
-  // { id: 23, title: "title 23", body: "body 23" },
-  // { id: 24, title: "title 24", body: "body 24" },
-  // { id: 25, title: "title 25", body: "body 25" },
-  // { id: 26, title: "title 26", body: "body 26" },
-  // { id: 27, title: "title 27", body: "body 27" },
-  // { id: 28, title: "title 28", body: "body 28" },
-  // { id: 29, title: "title 29", body: "body 29" },
-  // { id: 30, title: "title 30", body: "body 30" },
+  { id: 03, title: "Protect Your Accounts with Strong Passwords", body: "Make sure you always create new passwords with both upper and lower-case letters, numbers, and special characters. Also make sure you don't use the same password for multiple accounts. If a hacker gets one of your passwords, they will immediately try it everywhere." },
+  { id: 04, title: "Shut Your Computer Down Often", body: "Many common computer issues are a direct result of leaving your computer on too long. Shutting your computer down at least once a week is a great way to prevent problems." },
+  { id: 05, title: "Check Your Internet Speed", body: `Websites like <a target="_blank" href="http://speakeasy.net"><u>speakeasy.net</u></a> and <a target="_blank" href="http://speedtest.net"><u>speedtest.net</u></a> allow you to check your internet speed to make sure your network is functioning properly.` },
+  { id: 06, title: "Don't Click on Suspicious Links", body: "If you're not sure, play it safe! Your can always forward any suspicious sites and emails to support@INNYCIT.com and we will investigate on your behalf." },
+  { id: 07, title: "We're Here to Help", body: "If you have a small issue you think you can resolve, great! However, it is recommended to avoid trying to address any issues you aren't comfortable with. When in doubt, let IN NYC IT be your tech super-hero." },
+  { id: 08, title: "Make Websites Easier to Read", body: "If you're finding the text on a website too big or too small, you can press Cmd-+/- (Mac) or Cntrl-+/- (PC) to increase and decrease the font size in your web browser." },
+  { id: 09, title: "Re-Open a Closed Browser Tab ", body: "Have you ever accidentally closed a browser tab before you are done?<br>All is not lost! If you press Cmd-Shift-Tab (Mac) or Cntrl-Shift-Tab (PC) you can re-open the last tab you've closed." },
+  { id: 10, title: "Always Backup Your Files", body: "Services like G-Drive and Dropbox have native applications that automatically upload your files to the cloud. You should always save your work into the G-Drive or Dropbox folders if you have them. Any files on your desktop won't automatically backup. If you're not using one of these services, you may want to invest in an external hard-drive to peridoically backup your work." },
+  { id: 11, title: "Password Protect Your Wireless Network", body: "If your wireless network doesn't have a password, you are vulnerable to a host of serious cyber-threats. In a place as congested as NYC, you should never, ever have an unprotected wireless network." },
+  { id: 12, title: "Empty Your Downloads Folder", body: "Many people don't realize how many old files they have collecting dust in their downloads folder. Periodically emptying your downloads folder can save Gigabytes of storage space, especially if you haven't checked in a while." },
+  { id: 14, title: "Cmd-F (Mac) / Cntrl-F (PC)", body: `If you are looking for a specific keyword on a website, spreadsheet, Word document, or PDF you can use the built-in "Find" feature.<br>Pressing Cmd-F (Mac) or Cntrl-F (PC) will open a small window where you can type a keyword. The application will then highlight all instances of that keyword making your life much, much easier. ` },
+  { id: 15, title: "Search Google with a Right-Click", body: `While browsing the web, you can right-click on any word or selected text and click "Search Google". This will work in Safari, Chrome, and Firefox.` },
 ]
-
-
 
 window.addEventListener('load', function () {
   let randomTipId = Math.floor(Math.random() * tips.length)
@@ -46,9 +30,6 @@ window.addEventListener('load', function () {
   tipBody.innerHTML = `${tips[randomTipId].body}`
   console.log(randomTipId)
 });
-
-
-const button = document.getElementById('create-ticket')
 
 button.addEventListener('click', function () {
   chrome.tabs.update({
